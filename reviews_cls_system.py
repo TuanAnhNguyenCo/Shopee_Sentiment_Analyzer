81e8258b99a1e1a99060504f2640277d36fdebd5
Nguyen Co Tuan Anh <coanh2710@gmail.com>
2024-05-13 23:21:31 +0700

    add reviews classification system

---
+++ reviews_cls_system.py
@@ -0,0 +1,104 @@
+import re
+import json
+import requests
+import pandas as pd
+from tqdm.auto import tqdm
+from clean_data import CleanData
+import requests
+import sys
+
+# khi chạy code cần login shopee lấy cookies thay vào phần cookie
+# truy cập link https://shopee.vn/api/v2/item/get_ratings?filter=0&flag=1&itemid=11424337247&limit=20&offset=0&shopid=39682649&type=0
+headers = {
+    'authority': 'shopee.vn',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Accept-Encoding': 'gzip, deflate, br, zstd',
+    'Accept-Language': 'vi,vi-VN;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5',
+    'Cache-Control': 'max-age=0',
+    'Cookie': 'SPC_SI=TE0vZgAAAABBRWFsZVE2UpQQlAAAAAAAQ0Z0QlVDTjg=; SPC_SEC_SI=v1-RThMdUgwWVZTYXJEajh1OeVetya1oDW5FSCjWTWZRWL6qBT2SAMvv8qlQdKkMbIXxyn8rKgc4nMvir7BD/z4o/lLeIvBZqGFQKR1+C/Ysb8=; SPC_F=4wVsgvENFFS5Jp61U0BnGtl0WgwLdM84; REC_T_ID=d3e85524-0ab4-11ef-be27-5ef2fac23a29; __LOCALE__null=VN; csrftoken=VY4GDo5O4AJaFRHqzCfoInyHShHKQHuS; _sapid=fd9c00bf3ea337d9723ce66fc5d547780a3041204ee519be8686b923; _QPWSDCXHZQA=2e22e258-6806-4097-ca22-04fd8b892ed5; REC7iLP4Q=b9af0c29-a4b1-4c78-bc32-e342e037fdc1; SPC_CLIENTID=NHdWc2d2RU5GRlM1ymekwsezfaczwcsd; SPC_EC=.SDJxMDJOUm5CcU41NEFCOcpi0HTsW8oAGy6YFG3VwtpweO90UWXwCwrLO116rA9KV9OppDzDMYUaove32JmFB6u3O5UIDF4N55VU3/MRR232qZauoG1CPz4JrPQ8iOEaL3tdcDn8uW4Zn5nHceNEvJ9ClWgZZQbhvESVcwjqdL7exsg49Z8GKvfr6uGpvnL31X8V2B6TmvwbUXAbw4w2BQ==; SPC_ST=.SDJxMDJOUm5CcU41NEFCOcpi0HTsW8oAGy6YFG3VwtpweO90UWXwCwrLO116rA9KV9OppDzDMYUaove32JmFB6u3O5UIDF4N55VU3/MRR232qZauoG1CPz4JrPQ8iOEaL3tdcDn8uW4Zn5nHceNEvJ9ClWgZZQbhvESVcwjqdL7exsg49Z8GKvfr6uGpvnL31X8V2B6TmvwbUXAbw4w2BQ==; SPC_U=991218307; SPC_R_T_ID=rRvFPAjAW61phddGCWwHce0VCHcRV3Pwh62/GyaGwcuF83oqqY8StJxyVtsw3hJfIaqeK+VVXaMSOotHcJPjrP4ndDgnhElDmlLufYTDacdM7X2PSn/nE2AZRe6oC3+A6T6FN08EmBLSt2igov5r66Ti6VQLNta/Le2wSgPl+DA=; SPC_R_T_IV=aXFFeEdjM2Y4bGptWDFPOA==; SPC_T_ID=rRvFPAjAW61phddGCWwHce0VCHcRV3Pwh62/GyaGwcuF83oqqY8StJxyVtsw3hJfIaqeK+VVXaMSOotHcJPjrP4ndDgnhElDmlLufYTDacdM7X2PSn/nE2AZRe6oC3+A6T6FN08EmBLSt2igov5r66Ti6VQLNta/Le2wSgPl+DA=; SPC_T_IV=aXFFeEdjM2Y4bGptWDFPOA==; SPC_CDS_CHAT=26698a3c-d82d-43ae-86f2-5cbf30853723',
+    'Priority': 'u=0, i',
+    'Sec-Ch-Ua': '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
+    'Sec-Ch-Ua-Mobile': '?0',
+    'Sec-Ch-Ua-Platform': '"Windows"',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'none',
+    'Sec-Fetch-User': '?1',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
+}
+
+
+class Reviews_CLS_System:
+    def __init__(self,api = 'http://222.252.4.232:9999/classify_reviews' ):
+        self.clean = CleanData(
+            abbreviation_words_file_url='specialchar.txt', save_dir='./')
+        self.api = api
+
+    def get_rating_urls(self, url):
+        r = re.search(r"i\.(\d+)\.(\d+)", url)
+        self.shop_id, self.item_id = r[1], r[2]
+        self.ratings_url = "https://shopee.vn/api/v4/item/get_ratings?filter=0&flag=1&itemid={item_id}&limit=20&offset={offset}&shopid={shop_id}&type=0"
+
+    def get_data_and_predict_from_url(self, url):
+        self.data = []
+        self.ratings_url = None
+        self.get_rating_urls(url)
+        positive = 0
+        negative = 0
+        total = 0
+        cmts = []
+
+        assert self.ratings_url is not None
+        try:
+            for offset in tqdm(range(20,100000,20)): # max 100000 comments
+                data = requests.get(
+                    self.ratings_url.format(
+                        shop_id=self.shop_id, item_id=self.item_id, offset=offset), headers=headers).json()
+                for i, rating in enumerate(data["data"]["ratings"], 1):
+                    if rating["comment"] == "" :
+                        continue
+                    comment = self.clean.clean_text(rating["comment"])
+                    cmts.append(comment)
+        
+        except Exception as ex:
+            pass
+        print('*'*15,"CRAWLING REVIEWS: DONE!",'*'*15)
+        print('*'*15,"PREDICTING REVIEWS: STARTING!",'*'*15)
+        if len(cmts) != 0 :
+            bs = 16
+            total = len(cmts)
+            for index in range(0,len(cmts),bs):
+                sys.stdout.write(f"\rProgress: {min(index+16,total)}/{total}")
+                sys.stdout.flush()
+                data = {
+                    'text': '-----'.join(cmts[index:index+bs])
+                }
+                response = requests.post(self.api,params = data)
+                label = 0
+                output = response.json()['message'].split('-----')
+                for o in output:
+                    if o == 'positive':
+                        positive +=1
+                    elif o == 'negative':
+                        negative += 1
+            
+
+        return positive,negative,total
+        
+    def __call__(self,url):
+        positive,negative,total = self.get_data_and_predict_from_url(url)
+        print()
+        print('*'*15,"STATISTICS",'*'*15)
+        print("NUM POSITIVE:",positive)
+        print("NUM NEGATIVE:",negative)
+        print("TOTAL:",total)
+        if total != 0:
+            print("POSITIVE/TOTAL",positive/total)
+            print("NEGATIVE/TOTAL",negative/total)
+        print('*'*15,"END",'*'*15)
+        print()
+
+
+pipeline = Reviews_CLS_System(api = 'http://222.252.4.232:9999/classify_reviews')
+pipeline('https://shopee.vn/%C4%90%E1%BB%93ng-H%E1%BB%93-%C4%90eo-Tay-Th%E1%BB%9Di-Trang-M%E1%BA%B7t-S%E1%BB%91-%E1%BA%A2-R%E1%BA%ADp-D%C3%A0nh-Cho-N%E1%BB%AF-i.1006220775.24504892554?publish_id=&sp_atk=58e741c7-4234-41d2-93ba-37c3397f04d0&xptdk=58e741c7-4234-41d2-93ba-37c3397f04d0')
